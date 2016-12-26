b6e80ea1e370d55790dc3453b0b5a0b6a13178f7
Islam Kamilov <kamilov6@gmail.com>
2016-12-26 13:35:35 -0600

    Add files via upload

---
+++ CPU/src/cpu/CPU.java
@@ -0,0 +1,15 @@
+package cpu;
+
+/**
+ * @author Islam Kamilov
+ * Main class of the application that initializes Face Frame.
+ */
+public class CPU {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+            Face face = new Face();
+            face.show();
+      
+    }
+}
